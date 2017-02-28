219701934c3c8ab43fc87daeda846e2a050b669f
shekhar <shekharsingh099@gmail.com>
2017-02-28 21:18:54 +0530

    auth middleware addedd

--- app/Http/Controllers/AdminPostController.php
+++ app/Http/Controllers/AdminPostController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post, Session, Validator;
-class PostController extends Controller
+class AdminPostController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -13,7 +13,10 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $data = [];
+        $data['page'] = 'admin_post';
+        $data['posts'] = Post::orderBy('created_at', 'DESC')->paginate(10);
+        return view('admin', $data);
     }
 
     /**
@@ -23,17 +26,7 @@ class PostController extends Controller
      */
     public function create()
     {
-      $data = [];
-      $data['page'] = 'create_post';
-      return view('index', $data);
-    }
 
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'title' => 'required',
-            'post' => 'required'
-        ]);
     }
 
     /**
@@ -42,17 +35,15 @@ class PostController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+   protected function validator(array $data)
+   {
+
+   }
+
     public function store(Request $request)
     {
-      $validator = $this->validator($request->all());
 
-      if($validator->fails())
-      {
-          return Redirect::back()->withInput()->withErrors($validator);
-      }
-      Post::store($request);
-      Session::flash('message', 'Your post has been submited successfully.');
-      return back();
+
     }
 
     /**
@@ -98,5 +89,49 @@ class PostController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public function approvePost($id)
+    {
+      $post = Post::find($id);
+      if($post)
+      {
+          $post->is_approved = 1;
+          $post->save();
+          Session::flash('success', 'Post has been approved successfully.');
+      }
+      else
+      {
+          Session::flash('admin_error_message', 'Something went wrong with approval.');
+      }
+      return back();
+    }
+
+    public function getPost($id)
+    {
+      $data = [];
+      $data['page'] = 'admin_post_edit';
+      $data['post'] = Post::find($id);
+      return view('admin', $data);
+    }
+
+    public function deletePost($id)
+    {
+        $post = Post::find($id);
+        if($post) {
+            $post->delete();
+            Session::flash('success', 'Post has been deleted successfully.');
+            return redirect('/admin/posts');
+        }
+        Session::flash('admin_error_message', 'Something went wrong.');
+        return back();
+    }
+
+    public function viewPost($id)
+    {
+      $data = [];
+      $data['page'] = 'view_post';
+      $data['post'] = Post::find($id);
+      return view('admin', $data);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,9 @@ Route::post('login', 'Auth\LoginController@login');
 Route::get('register', 'Auth\RegisterController@getRegister');
 Route::post('register', 'Auth\RegisterController@register');
 Route::get('logout', 'Auth\LoginController@logout');
-Route::group(['prefix'=>''], function() {
-    Route::resource('/', 'IndexController');
+Route::resource('/', 'IndexController');
+Route::group(['prefix'=>'', 'middleware'=>['auth']], function() {
+
     Route::resource('profile', 'ProfileController');
 });
 
@@ -26,22 +27,27 @@ Route::group(['prefix'=>''], function() {
   Route::resource('post', 'PublicPostController');
 });
 
-Route::group(['prefix'=>''], function() {
-  Route::get('admin/posts', 'PostController@index');
-  Route::get('admin/posts/{id}/approve', 'PostController@approvePost');
-  Route::get('admin/posts/{id}/edit', 'PostController@getPost');
-  Route::get('admin/posts/{id}/delete', 'PostController@deletePost');
-  Route::get('admin/posts/{id}/view', 'PostController@viewPost');
-  Route::resource('posts', 'PostController');
+Route::group(['prefix'=>'admin', 'middleware'=>['auth','admin']], function() {
+  Route::get('posts', 'AdminPostController@index');
+  Route::get('posts/{id}/approve', 'AdminPostController@approvePost');
+  Route::get('posts/{id}/edit', 'AdminPostController@getPost');
+  Route::get('posts/{id}/delete', 'AdminPostController@deletePost');
+  Route::get('posts/{id}/view', 'AdminPostController@viewPost');
 });
 
-Route::group(['prefix'=>'admin'], function(){
+Route::group(['prefix'=>''], function() {
+
+  Route::resource('posts', 'PostController');
+
+});
+
+Route::group(['prefix'=>'admin', 'middleware'=>['auth','admin']], function(){
   Route::resource('/', 'AdminController');
   Route::get('users/{id}/delete', 'UserManageController@destroy');
   Route::resource('users', 'UserManageController');
 });
 
-Route::group(['prefix'=>'admin'], function(){
+Route::group(['prefix'=>'admin', 'middleware'=>['auth','admin']], function(){
   Route::get('comments/{id}/approve', 'AdminCommentController@approveComment');
   Route::get('comments/{id}/unapprove', 'AdminCommentController@UnapproveComment');
   Route::get('comments/{id}/delete', 'AdminCommentController@deleteComment');
@@ -52,8 +58,7 @@ Route::group(['prefix'=>'admin'], function(){
 
   Route::resource('comments', 'AdminCommentController');
 });
+
 Route::get('register/verify/{confirmationCode}', 'Auth\RegisterController@confirm');
 Auth::routes();
-
-
-Route::get('/home', 'HomeController@index');
+ Route::get('/home', 'HomeController@index');
